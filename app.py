fa72aaa734674906eaf0eb50e579d7c3cd1ef229
AmitKumarSinghAI <amitkumarsinghkurmi123@gmail.com>
2025-11-02 11:15:03 +0530

    pushing

---
+++ app.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
+from fastapi.responses import JSONResponse
+import pandas as pd
+import pickle
+from pymongo import MongoClient
+
+try:
+    client = MongoClient('mongodb://localhost:27017/')
+    database = client['Insurance']
+    collection = database["users"]
+
+except Exception as e:
+    print(f'failed to connect to the database {e}')
+
+
+
+
+
+tier_1_cities = [
+    "Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"
+]
+
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = FastAPI()
+
+class UserInput(BaseModel):
+    age: Annotated[int, Field(..., gt=0, lt=200, description='Age of user')]
+    weight: Annotated[int, Field(..., gt=5, description='Weight of the user')]
+    height: Annotated[float, Field(..., description='Height of the user')]
+    income_lpa: Annotated[float, Field(..., description='Income of the user')]
+    smoker: Annotated[bool, Field(..., description='Smoker or not')]
+    city: Annotated[str, Field(..., description='The user belongs from')]
+
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
+
+    @computed_field(return_type=float)
+    @property
+    def bmi(self) -> float:
+        return self.weight / (self.height ** 2)
+
+    @computed_field(return_type=str)
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return 'young'
+        elif self.age < 45:
+            return 'adult'
+        elif self.age < 60:
+            return 'middle_aged'
+        return 'senior'
+
+    @computed_field(return_type=str)
+    @property
+    def life_style(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return 'high'
+        elif self.smoker or self.bmi > 27:
+            return 'medium'
+        else:
+            return 'low'
+
+    @computed_field(return_type=int)
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+
+@app.get('/')
+def homepage():
+    return {'message': '🏠 Welcome to the Insurance Premium Prediction API. Use POST /predict to get predictions.'}
+
+
+@app.post("/predict")
+def premium_predict(data: UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.life_style,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    record = data.model_dump()
+    record['predicted_category'] = prediction
+    collection.insert_one(record)
+
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
